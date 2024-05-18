813b3ee9daa857cbe2bc03749130dd66c4010a10
Madina <69770324+matonyn@users.noreply.github.com>
2024-05-19 00:07:11 +0500

    Add files via upload

---
+++ spoty_task/spoty/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User, auth
+import random
+
+# Create your models here.
+
+class song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    duration = models.DurationField()
+    release_date = models.DateField()
+    ranking = models.FloatField( default=0.0)
+    id = models.PositiveIntegerField(default=random.randint(1,10000), primary_key=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.artist}"
+
+class reviews(models.Model):
+    song = models.ForeignKey(song, on_delete=models.CASCADE)
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user}"
